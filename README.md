b754f44400c14a05cf875af3cc2e511163a26adf
Ray <raymund.lew@gmail.com>
2014-06-26 15:05:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tulum Countdown
+===============
+
+Meteor app I created one afternoon to count down the seconds until vacation in Tulum!
+Uses Meteor with jQuery and nothing else.
---
+++ tulumcountdown.js
@@ -0,0 +1,45 @@
+if (Meteor.isClient) {
+  Countdown = function() {
+    var now = new Date();
+    var leaveSFO = new Date("July 3, 2014 13:25:00");
+    var millisTilTakeoff = leaveSFO - now;
+    this.daysTilTakeoff = Math.floor(millisTilTakeoff / (24 * 60 * 60 * 1000));
+    this.hoursTilTakeoff = Math.floor(
+      (millisTilTakeoff - (this.daysTilTakeoff * 24 * 60 * 60 * 1000)) /
+      (60 * 60 * 1000)
+    );
+    this.minutesTilTakeoff = Math.floor(
+      (millisTilTakeoff - (this.daysTilTakeoff * 24 * 60 * 60 * 1000) -
+      (this.hoursTilTakeoff * 60 * 60 * 1000)) /
+      (60 * 1000)
+    );
+    this.secondsTilTakeoff = Math.floor(
+      (millisTilTakeoff - (this.daysTilTakeoff * 24 * 60 * 60 * 1000) -
+      (this.hoursTilTakeoff * 60 * 60 * 1000) -
+      (this.minutesTilTakeoff * 60 * 1000)) /
+      1000
+    );
+  }
+  clockDisplay = function() {
+    var countdown = new Countdown();
+    $('#clock').html(
+      countdown.daysTilTakeoff + " days " +
+      countdown.hoursTilTakeoff + " hours " +
+      countdown.minutesTilTakeoff + " minutes " +
+      countdown.secondsTilTakeoff + " seconds "
+    );
+  }
+  Meteor.startup(function () {
+    clockDisplay();
+    Meteor.setInterval(function() {
+      clockDisplay();
+    }, 1000);
+    $('#clock').click(function() {
+      $(this).addClass('spin');
+      Meteor.setTimeout(function() {
+        $('#clock').removeClass('spin');
+      }, 600);
+    })
+  });
+}
+
